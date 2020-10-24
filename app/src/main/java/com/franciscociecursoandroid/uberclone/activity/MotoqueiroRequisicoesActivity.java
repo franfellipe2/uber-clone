b4e6dd62a89cb587524b9a1744b00d96cff2cbf6
Francisco S. Filho <franfellipe2@hotmail.com>
2020-10-24 20:21:37 -0300

    Direcionar usuario logado para a tela motorista/passageiro

---
+++ app/src/main/java/com/franciscociecursoandroid/uberclone/activity/MotoqueiroRequisicoesActivity.java
@@ -0,0 +1,16 @@
+package com.franciscociecursoandroid.uberclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.franciscociecursoandroid.uberclone.R;
+
+public class MotoqueiroRequisicoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motoqueiro_requisicoes);
+    }
+}
